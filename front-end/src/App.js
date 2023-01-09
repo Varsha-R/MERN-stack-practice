993b78bde9c544d8d327f0d08fee069a194febec
Varsha-R <varsharavindra12@gmail.com>
2023-01-09 11:59:44 -0800

    Added log in (authentication) functionality

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,6 +11,7 @@ import UserPlaces from "./places/pages/UserPlaces";
 import Users from "./user/pages/Users";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 
 const App = () => {
   return (
@@ -30,6 +31,9 @@ const App = () => {
           </Route>
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>
