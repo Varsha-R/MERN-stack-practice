7882527105a4fb7a5734acb30cf75e7251e9ada9
Varsha-R <varsharavindra12@gmail.com>
2023-01-09 22:08:20 -0800

    Connecting to mongoose and adding a collection

--- back-end/controllers/places-controllers.js
+++ back-end/controllers/places-controllers.js
@@ -2,6 +2,7 @@ const { v4: uuid } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
+const Place = require("../models/place");
 
 let DUMMY_PLACES = [
   {
@@ -49,7 +50,7 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
@@ -57,16 +58,25 @@ const createPlace = (req, res, next) => {
   }
 
   const { title, description, coordinates, address, creator } = req.body;
-  const createdPlace = {
-    id: uuid(),
-    title: title,
-    description: description,
+  const createdPlace = new Place({
+    title,
+    description,
+    address,
     location: coordinates,
-    address: address,
-    creator: creator,
-  };
+    image:
+      "https://www.shainblumphoto.com/wp-content/uploads/2015/11/sffoggynightggb1200.jpg",
+    creator,
+  });
 
-  DUMMY_PLACES.push(createdPlace);
+  try {
+    await createdPlace.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Creating place failed, try again + " + err,
+      500
+    );
+    return next(error);
+  }
 
   res.status(201).json({ place: createdPlace });
 };
