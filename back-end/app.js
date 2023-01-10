dcde7cfb5296e5ac551a5e99cb0172b7e835ad1b
Varsha-R <varsharavindra12@gmail.com>
2023-01-09 18:04:09 -0800

    Added PATCH and DELETE methods

--- back-end/app.js
+++ back-end/app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes); // => /api/places/...
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route", 404);
+  throw error;
+}); // Only reached if there is a request which doesn't have a response
 
 // Special middleware func for error - should be after above declaration
 app.use((error, req, res, next) => {
